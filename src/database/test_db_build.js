56d3b15b78249de1b4054070b89114a48b3d4c13
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-04-13 05:12:46 +0100

    test signUpHandler

--- src/database/test_db_build.js
+++ src/database/test_db_build.js
@@ -6,8 +6,7 @@ const sql = fs.readFileSync(`${__dirname}/db_build.sql`).toString();
 const buildTestDb = (cb) => {
     dbConnection.query(sql, (err, res) => {
         if (err) cb(err);
-        cb(null, res);
-        dbConnection.end();
+        cb(null, res, dbConnection);
     })
 }
 
--- src/tests/handler.test.js
+++ src/tests/handler.test.js
@@ -1,11 +1,29 @@
-require('env2')('./.env');
-const { TEST_PASSWORD } = process.env
 const test = require('tape');
 const supertest =  require('supertest');
 const buildTestDb = require('../database/test_db_build');
 
+const { signUpHandler } = require('../handler')
+
+const { log } = console;
+const TEST_PASSWORD = 'Password123456!';
 
 test('Tape is working', (t) => {
   t.ok(true, 'tape is working');
   t.end();
+})
+
+test('Test signUpHandler', (t) => {
+
+  buildTestDb((err, res, dbConnection) => {
+    supertest(signUpHandler)
+    .post('/post')
+    .send('username=lawrence')
+    .send(`password=${TEST_PASSWORD}`)
+    .end((err, res) => {
+      t.error(err, 'user successfully added to database');
+      t.end();
+      dbConnection.end();
+    });
+  });
+  
 })
