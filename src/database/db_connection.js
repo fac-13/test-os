93fd56b1fff01972f7022f8d449b0fb3b2f06fd3
Lawrence Sarpong <lawrenceeks@yahoo.com>
2018-04-13 05:26:49 +0100

    set up travis test env

--- src/database/db_connection.js
+++ src/database/db_connection.js
@@ -12,7 +12,7 @@ if (!DB_URL) throw new Error("Environment variable DB_URL must be set");
 const params = url.parse(DB_URL);
 const [user, password] = params.auth.split(':');
 
-const options = {
+let options = {
     host: params.hostname,
     port: params.port,
     database: params.pathname.split("/")[1],
@@ -22,5 +22,12 @@ const options = {
 };
 
 options.ssl = options.host !== "localhost";
+
+if (process.env.TRAVIS === 'true') {
+    options = {
+        database: 'travis_ci_test',
+        user: 'postgres'
+    }
+}
 
 module.exports = new Pool(options); 
